b8cffb11c9fd38dc33d822911fad5b12a34e2539
Tina <valentinamodyrka1@gmail.com>
2025-05-08 10:49:16 -0600

    update

---
+++ js/task-2.js
@@ -0,0 +1,39 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const newItems = [];
+
+    for (let i = 0; i < this.#items.length; i++) {
+      if (this.#items[i] !== itemToRemove) {
+        newItems.push(this.#items[i]);
+      }
+    }
+
+    this.#items = newItems;
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
